aa4071215eb7db45199d99832015261e2ce7ef84
ce-admin-dashboard <creativeparadise25@gmail.com>
2026-07-29 22:21:22 +0530

    Disable Popunder click-hijacking to ensure smooth user navigation and UX

--- components/MonetagAds.tsx
+++ components/MonetagAds.tsx
@@ -5,29 +5,31 @@ import Script from 'next/script';
 export default function MonetagAds() {
   return (
     <>
-      {/* 1. Monetag Vignette / Interstitial Ad (Zone: 11453477) */}
+      {/* 1. Monetag Vignette / Interstitial Ad (Zone: 11453477) - High CPM on Download/Actions */}
       <Script
         id="monetag-vignette-11453477"
         src="https://n6wxm.com/vignette.min.js"
         data-zone="11453477"
-        strategy="afterInteractive"
+        strategy="lazyOnload"
       />
 
-      {/* 2. Monetag OnClick / Popunder Ad (Zone: 11453486) */}
-      <Script
-        id="monetag-popunder-11453486"
-        src="https://al5sm.com/tag.min.js"
-        data-zone="11453486"
-        strategy="afterInteractive"
-      />
-
-      {/* 3. Monetag In-Page Push Ad (Zone: 11453490) */}
+      {/* 2. Monetag In-Page Push Ad (Zone: 11453490) - Clean Native Banner Ads */}
       <Script
         id="monetag-ipp-11453490"
         src="https://nap5k.com/tag.min.js"
         data-zone="11453490"
-        strategy="afterInteractive"
+        strategy="lazyOnload"
       />
+
+      {/* OnClick Popunder disabled to protect Mobile User Experience & Navigation */}
+      {/* 
+      <Script
+        id="monetag-popunder-11453486"
+        src="https://al5sm.com/tag.min.js"
+        data-zone="11453486"
+        strategy="lazyOnload"
+      /> 
+      */}
     </>
   );
 }
